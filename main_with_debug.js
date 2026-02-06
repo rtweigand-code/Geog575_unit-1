d43424ca804f4812cb8de032ad3733cedfc4f18c
rtweigand-code <rtweigand@wisc.edu>
2026-02-05 19:31:22 -0600

    Fix debugging issues in city population table

--- main_with_debug.js
+++ main_with_debug.js
@@ -1,74 +1,122 @@
+// Activity 3 - Cities Table + Debugging (fixed)
+// goal: make the table draw + add hover + click interactions
+
+// data (array of objects)
 var cityPop = [
-	{ 
-		city: 'Madison',
-		population: 233209
-	},
-	{
-		city: 'Milwaukee',
-		population: 594833
-	},
-	{
-		city: 'Green Bay',
-		population: 104057
-	},
-	{
-		city: 'Superior',
-		population: 27244
-	}
+  {
+    city: "Madison",
+    population: 233209
+  },
+  {
+    city: "Milwaukee",
+    population: 594833
+  },
+  {
+    city: "Green Bay",
+    population: 104057
+  },
+  {
+    city: "Superior",
+    population: 27244
+  }
 ];
 
-function addColumns(cityPop){
-    
-    document.querySelectorAll("tr").forEach(function(row, i){
+// runs when the DOM is ready
+function initialize() {
+  cities();
+}
 
-    	if (i == 0){
+// builds the table + calls the other functions
+function cities() {
+  // make the table element
+  var table = document.createElement("table");
 
-    		row.insertAdjacntHTML('beforeend', '<th>City Size</th>');
-    	} else {
+  // header row
+  var headerRow = document.createElement("tr");
+  headerRow.insertAdjacentHTML("beforeend", "<th>City</th><th>Population</th>");
+  table.appendChild(headerRow);
 
-    		var citySize;
+  // add rows for each city
+  for (var i = 0; i < cityPop.length; i++) {
+    var rowHtml =
+      "<tr><td>" +
+      cityPop[i].city +
+      "</td><td>" +
+      cityPop[i].population +
+      "</td></tr>";
 
-    		if (cityPop[i-1].population < 100000){
-    			citySize = 'Small';
+    table.insertAdjacentHTML("beforeend", rowHtml);
+  }
 
-    		} else if (cityPop[i-1].population < 500000){
-    			citysize = 'Medium';
+  // add table to the div in index.html
+  document.querySelector("#myDiv").appendChild(table);
 
-    		} else {
-    			citySize = 'Large';
-    		};
+  // add the extra column + the interactions
+  addColumns(cityPop);
+  addEvents();
+}
 
-			row.insertAdjacntHTML = '<td' + citySize + '</td>';
-    	};
+// adds a 3rd column called "City Size"
+function addColumns(cityPop) {
+  // loop through each table row
+  document.querySelectorAll("tr").forEach(function (row, i) {
+    // first row is the header
+    if (i === 0) {
+      row.insertAdjacentHTML("beforeend", "<th>City Size</th>");
+    } else {
+      var citySize;
+
+      // cityPop index is i-1 because header row is i=0
+      if (cityPop[i - 1].population < 100000) {
+        citySize = "Small";
+      } else if (cityPop[i - 1].population < 500000) {
+        citySize = "Medium";
+      } else {
+        citySize = "Large";
+      }
+
+      // add the new td cell at the end of the row
+      row.insertAdjacentHTML("beforeend", "<td>" + citySize + "</td>");
+    }
+  });
+}
+
+// hover + click interactions
+function addEvents() {
+  // add events to each non-header row
+  document.querySelectorAll("tr").forEach(function (row, i) {
+    if (i === 0) return; // skip header
+
+    // hover: random background color
+    row.addEventListener("mouseover", function () {
+      var color = "rgb(";
+
+      for (var j = 0; j < 3; j++) {
+        var random = Math.round(Math.random() * 255);
+        color += random;
+
+        if (j < 2) {
+          color += ",";
+        } else {
+          color += ")";
+        }
+      }
+
+      row.style.backgroundColor = color;
+      row.style.cursor = "pointer";
     });
-};
 
-function addEvents(){
+    // un-hover: reset background
+    row.addEventListener("mouseout", function () {
+      row.style.backgroundColor = "";
+    });
 
-	document.querySelector("table").addEventListener("mouseover", function(){
-		
-		var color = "rgb(";
+    // click: simple alert
+    row.addEventListener("click", function () {
+      alert("Hey, you clicked a row!");
+    });
+  });
+}
 
-		for (var i=0; i<3; i++){
-
-			var random = Math.round(Math.random() * 255);
-
-			color += "random";
-
-			if (i<2){
-				color += ",";
-			
-			} else {
-				color += ")";
-		};
-
-		document.querySelector("table").color = color;
-	});
-
-	function clickme(){
-
-		alert('Hey, you clicked me!');
-	};
-
-	document.querySelector("table").addEventListener("click", clickme)
-};
+// call initialize when the DOM has loaded
+document.addEventListener("DOMContentLoaded", initialize);
